3dbe081f46ad2243b383b101ac6c75b78b742bf6
Guillaume Girol <symphorien+git@xlumurb.eu>
2023-04-08 12:00:00 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# nixpkgs-staging-bisecter
+
+`git bisect` is designed to find the commit introducing a regression within a
+range by visiting the least commits. This is very handy to bisect regressions
+in nixpkgs as long as everything is in the cache. However, `git bisect` sometimes
+picks a commit introducing a mass rebuild.
+
+One strategy is to override git's pick by choosing a nearby commit built by
+hydra, see [hydrasect](https://git.qyliss.net/hydrasect/about/) for example.
+But at some point, you may end up bisecting inside a large range of commits
+that where not built by hydra, for example staging. `nixpkgs-staging-bisecter`
+is designed to spend some time choosing a better commit than `git bisect` by
+attempting to minimize how many derivations you will build instead of the number
+of visited commits.
+
+This is only interesting when you expected to spend much more time building than
+testing.
+
+## Example
+I am looking for a regression only visible in a vm built by 
+```
+nixos-rebuild build-vm -I nixos-config=temp/vm.nix -I nixpkgs=. --fast -j2
+```
+
+`nixpkgs-staging-bisecter` only supports commands starting by `nix-build` so here is the `nix-build` equivalent:
+```
+nix-build '<nixpkgs/nixos>' -A vm -I nixos-config=temp/vm.nix -I nixpkgs=. -j2
+```
+
+Additionally files not checked in nixpkgs must be referred as absolute path (the command will be run in other directories by `bisecter.py`):
+```
+nix-build '<nixpkgs/nixos>' -A vm -I nixos-config=/home/symphorien/src/nixpkgs/temp/vm.nix -I nixpkgs=. -j2
+```
+
+Then every time you run `git bisect good` or `git bisect bad`, run
+```
+bisecter.py --checkout nix-build '<nixpkgs/nixos>' -A vm -I nixos-config=/home/symphorien/src/nixpkgs/temp/vm.nix -I nixpkgs=. -j2
+```
+
+It will pick a commit that ensures that if you build 
+`nixos-rebuild build-vm -I nixos-config=temp/vm.nix -I nixpkgs=. --fast -j2`,
+the expected number of rebuilds left for the remainder of the bisection is
+minimal, assuming the bug is uniformly distributed among possible commits.
+
+The first run will run the specified nix-build command with `--dry-build` on all commits in the
+git bisect range to determine rebuild counts, so this will be very slow, but
+later calls use a cache and should not take more than a minute.
+
+If you change the behavior of the command during the bisection (in my example by changing `temp/vm.nix`) you must invalidate the cache by nuking `$XDG_CACHE_HOME/bisecter`.
+
+## Installing
+
+`./bisecter.py` is just a python script with no dependencies.
+
---
+++ bisecter.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+import hashlib
+from pathlib import Path
+from ast import literal_eval
+from typing import Optional, Set
+from contextlib import contextmanager
+from tempfile import TemporaryDirectory
+from functools import reduce
+import os
+import argparse
+
+
+def run(cmd: list[str], verbose=True, **kwargs) -> subprocess.CompletedProcess:
+    """wrapper around subprocess.run that prints the command if verbose=True and fails if command fails"""
+    if verbose:
+        print("Running", " ".join(cmd))
+    return subprocess.run(cmd, check=True, **kwargs)
+
+
+def get_outputs(drv: str) -> list[str]:
+    """gets the output paths of a drv file"""
+    with open(drv) as f:
+        txt = f.read()
+    start = txt.index("[")
+    txt = txt[start:]
+    end = txt.index("]")
+    txt = txt[: end + 1]
+    data = literal_eval(txt)
+    return [i[1] for i in data]
+
+
+def is_built(drv: str) -> bool:
+    """determines wether a drv file has been build locally
+
+    actually whether its build outputs are present in the store
+    """
+    for line in get_outputs(drv):
+        assert line.startswith("/nix/store")
+        if Path(line).exists():
+            return True
+    return False
+
+
+def get_good_refs() -> list[str]:
+    """returns all references in the form refs/bisect/good-*"""
+    repo = Path(".git")
+    return [str(i.relative_to(repo)) for i in (repo / "refs" / "bisect").glob("good-*")]
+
+
+def git_rev_parse(rev: str) -> str:
+    """canonicalize a reference to a commit hash"""
+    return (
+        run(["git", "rev-parse", rev], verbose=False, stdout=subprocess.PIPE)
+        .stdout.decode("utf8", errors="ignore")
+        .strip()
+    )
+
+
+def get_bisect_commits(bad: str, goods: list[str]) -> list[str]:
+    """returns all commits that git bisect may visit if refs/bisect/bad is `bad` and refs/bisect-good* are `goods`, as commit hashes"""
+    bad = git_rev_parse(bad)
+    out = run(
+        ["git", "log", "--pretty=oneline", bad, "--not"] + goods,
+        verbose=False,
+        stdout=subprocess.PIPE,
+    ).stdout.decode("utf8", errors="ignore")
+    res = []
+    for line in out.splitlines():
+        if not line:
+            continue
+        words = line.strip().split(" ")
+        commit = words[0]
+        if commit != bad:
+            res.append(commit)
+    return res
+
+
+def hash(string: str) -> str:
+    """hashes a string"""
+    m = hashlib.sha256()
+    m.update(string.encode("utf8"))
+    return m.hexdigest()
+
+
+def cache_file_for(hash: str) -> Path:
+    """returns the path to a cache file depending on this hash only
+
+    its parent dir is guaranteed to exist
+    """
+    cachedir = os.environ.get("XDG_CACHE_HOME", os.environ["HOME"] + "/.cache")
+    d = Path(cachedir) / "bisecter"
+    d.mkdir(exist_ok=True)
+    f = d / hash
+    return f
+
+
+def cache_for(hash: str) -> Optional[str]:
+    """returns the value cached for this hash, if any"""
+    f = cache_file_for(hash)
+    if f.exists():
+        with f.open() as t:
+            return t.read()
+    return None
+
+
+def write_cache_for(hash: str, value: str):
+    """stores a value in on-disk cache for this hash"""
+    f = cache_file_for(hash)
+    with f.open("w") as t:
+        t.write(value)
+
+
+drv_re = re.compile(r"/nix/store/[^/ ]*\.drv")
+
+
+def get_drvs_inner(commit: str, cmd: list[str]) -> Set[str]:
+    """check-out this commit, and runs this command with --dry-run and parses all drvs that would be built"""
+    run(["git", "checkout", commit])
+    out = run(cmd + ["--dry-run"], stderr=subprocess.PIPE).stderr.decode(
+        "utf8", errors="ignore"
+    )
+    return set(drv_re.findall(out))
+
+
+def get_drvs(commit: str, cmd: list[str]) -> Set[str]:
+    """check-out this commit, and runs this command with --dry-run and parses all drvs that would be built
+
+    memoized
+    """
+    h = hash(commit + ";" + ";".join(cmd))
+    t = cache_for(h)
+    if t is not None:
+        v = literal_eval(t)
+    else:
+        v = get_drvs_inner(commit, cmd)
+        write_cache_for(h, repr(v))
+    return {drv for drv in v if not is_built(drv)}
+
+
+@contextmanager
+def worktree():
+    """context manager that sets up a temporary git worktree at HEAD"""
+    pwd = os.getcwd()
+    exc = None
+    with TemporaryDirectory() as tmpdir:
+        f = str(tmpdir) + "/w"
+        run(["git", "worktree", "add", f, "HEAD"])
+        os.chdir(f)
+        try:
+            yield
+        except BaseException as e:
+            exc = e
+    os.chdir(pwd)
+    run(["git", "worktree", "prune"])
+    if exc:
+        raise exc
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""computes good commits to build next in mass-rebuild-heavy git bisect (best first).
+
+        Contrary to git bisect, this programs attempts to minimize the number
+        of rebuilds the specified command needs instead of the number of
+        visited commits. It runs the command with --dry-run on every commits in
+        the git bisect range so it is notably slower than other helpers like
+        hydrasect. Actually you should probably only use this program when
+        hydrasect returns no good revision.
+
+        The command is assumed to only read files checked out in git for
+        aggressive caching; if it reads other files and that you change these
+        files between runs of this program, nuke $XDG_CACHE_HOME/bisecter.
+
+        """
+    )
+    parser.add_argument(
+        "cmd",
+        nargs=argparse.REMAINDER,
+        help="""Command that builds the desired derivation. must accept an
+        additional --dry-run flag, and output the drv files that would be built
+        on stderr. In practice this must be nix-build or a wrapper of it.""",
+    )
+    parser.add_argument(
+        "--checkout",
+        help="checkout best candidate after computation",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    if len(args.cmd) == 0:
+        print("no command given")
+        exit(1)
+    goods = get_good_refs()
+    bad = "refs/bisect/bad"
+    commits = get_bisect_commits(bad=bad, goods=goods)
+    n = len(commits)
+    print("found", n, "commits")
+    rebuilds = {}
+    with worktree():
+        for commit in commits:
+            rebuilds[commit] = get_drvs(commit, args.cmd)
+    weights = []
+    for commit in commits:
+        candidates_if_good = get_bisect_commits(bad=bad, goods=goods + [commit])
+        candidates_if_bad = get_bisect_commits(bad=commit, goods=goods)
+        rebuilds_if_good = len(
+            reduce(lambda a, b: a | b, (rebuilds[i] for i in candidates_if_good), set())
+            - rebuilds[commit]
+        )
+        rebuilds_if_bad = len(
+            reduce(lambda a, b: a | b, (rebuilds[i] for i in candidates_if_bad), set())
+            - rebuilds[commit]
+        )
+        w = (
+            len(rebuilds[commit])
+            + (
+                len(candidates_if_good) * rebuilds_if_good
+                + len(candidates_if_bad) * rebuilds_if_bad
+            )
+            / n
+        )
+        weights.append(
+            (
+                w,
+                commit,
+                len(candidates_if_good),
+                len(candidates_if_bad),
+                rebuilds_if_good,
+                rebuilds_if_bad,
+            )
+        )
+    weights.sort()
+    print(
+        "commit                                          ",
+        "estimated cost",
+        "rebuilds",
+        "commits>",
+        "rebuilds>",
+        "commits<",
+        "rebuilds<",
+        sep="\t",
+    )
+    for (
+        score,
+        commit,
+        candidates_if_good,
+        candidates_if_bad,
+        rebuilds_if_good,
+        rebuilds_if_bad,
+    ) in weights[:5]:
+        print(
+            commit,
+            int(score),
+            len(rebuilds[commit]),
+            candidates_if_good,
+            rebuilds_if_good,
+            candidates_if_bad,
+            rebuilds_if_bad,
+            sep="\t\t",
+        )
+    if args.checkout:
+        run(["git", "checkout", weights[0][1]])
